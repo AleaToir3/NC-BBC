d4d3fa0e06aef12f0494f91f285f87023ea1d6fd
AleaToir3 <aleatoir3@gmail.com>
2023-01-18 11:55:19 +0000

    forget task3 model

--- models.js
+++ models.js
@@ -6,44 +6,6 @@ const dataGetTopics = () =>{
  })
 }
 
-const dataGetArticles = ()=>{
-   // return db.query(`SELECT COUNT(*) as comment_count FROM comments WHERE article_id = 1;
-   return db.query(`
-   SELECT 
-      articles.author, 
-      articles.title, 
-      articles.article_id, 
-      articles.topic, 
-      articles.votes,
-      articles.created_at,
-      articles.article_img_url,
-   COUNT(comments.article_id) AS comment_count
-   FROM articles 
-   LEFT JOIN comments ON articles.article_id = comments.article_id
-   GROUP BY articles.article_id
-   ORDER BY comment_count DESC;
-   `)
-   .then((articles)=>{
-         return articles.rows
-      }
-   )
-}
-
-const dataGetArticleById = (articleId) => {
-
-   // const query = `SELECT body,author as username FROM articles WHERE article_id=$1;`
-   const query = `SELECT * FROM articles WHERE article_id=$1;`
-   return db.query(query,[articleId]).then((article)=>{
-      return article.rows
-   });
-}
-
-const dataCommentsByArticleId = (articleId)=>{
-   const query = `SELECT * FROM comments WHERE article_id = $1 ORDER BY created_at DESC;`;
-   return db.query(query,[articleId]).then((commentsByArticle)=>{
-      return commentsByArticle.rows
-   });
-}
 
 
 
