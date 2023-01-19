db4805fbdda6e5de74307e5bea231adab02afdf7
AleaToir3 <63627769+AleaToir3@users.noreply.github.com>
2023-01-19 13:55:10 +0000

    Update controllers.js

--- controllers.js
+++ controllers.js
@@ -1,7 +1,7 @@
 const {dataGetTopics} = require("./models")
 
 // TASK 3
-const getTopics = (req,res)=>{
+const getTopics = ()=>{
     return dataGetTopics(req,res).then(
         (datatopics) => {
             res.status(200).send({topics: datatopics})
