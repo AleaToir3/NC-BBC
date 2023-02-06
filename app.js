fecc9b721d80950300d74a68e82631337fc04e15
AleaToir3 <aleatoir3@gmail.com>
2023-02-06 11:20:30 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,66 +1,61 @@
-const express = require('express');
-const {getTopics,getArticles,getArticleById,getCommentsArticle,getUsers,
-       postCommentByArticleId,
-       patchVote,
-} = require('./controllers');
-const {errBadRequest,errTopics,errComments} = require('./errBBC');
-
+const express = require("express");
+const cors = require('cors');
+const {
+  getTopics,
+  getArticles,
+  getArticleById,
+  getCommentsArticle,
+  getUsers,
+  postCommentByArticleId,
+  patchVote,
+} = require("./controllers");
+const { errBadRequest, errTopics, errComments } = require("./errBBC");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
-                                                            
-
 // ==================   GET   ==================
-       //  -----    3    ------
-app.get("/api/topics",getTopics);
+//  -----    3    ------
+app.get("/api/topics", getTopics);
 
-       //  -----    4    ------
-app.get("/api/articles",getArticles);
-       
+//  -----    4    ------
+app.get("/api/articles", getArticles);
 
-       //  -----    5 & 11    ------
-app.get("/api/articles/:article_id",getArticleById)
+//  -----    5 & 11    ------
+app.get("/api/articles/:article_id", getArticleById);
 
-       //  -----    6    ------
- //🥸🥸🥸🥸🥸🥸🥸🥸🥸🥸🥸🥸🥸🥸🥸🥸🥸🥸🥸🥸🥸🥸
-//🥸🥸🥸🥸🥸🥸🥸🥸🥸🥸🥸🥸🥸🥸🥸🥸🥸🥸🥸🥸🥸🥸
-//🥸🥸🥸🥸🥸🥸🥸🥸🥸🥸🥸🥸🥸🥸🥸🥸🥸🥸🥸🥸🥸🥸
-app.get("/api/articles/:article_id/comments",getCommentsArticle)
+//  -----    6    ------
+app.get("/api/articles/:article_id/comments", getCommentsArticle);
 
-      //  -----    9    ------
-app.get("/api/users",getUsers)
+//  -----    9    ------
+app.get("/api/users", getUsers);
 
-       //  -----    4    ------
-app.get("/api/articles",getArticles);
-
+//  -----    4    ------
+app.get("/api/articles", getArticles);
 
 // ==================   POST   ==================
 
-       //  -----    7    ------
-app.post("/api/articles/:article_id/comments",postCommentByArticleId)
+//  -----    7    ------
+app.post("/api/articles/:article_id/comments", postCommentByArticleId);
 
 // ==================   PATCH   ==================
 
-
 //  -----    8    ------
-app.patch("/api/articles/:article_id",patchVote)
+app.patch("/api/articles/:article_id", patchVote);
 
-      //  -----    10    ------
-      //  -----    12    ------
+//  -----    10    ------
+//  -----    12    ------
 
 // HANDLING MIDLLEWARE ERRORS
 // app.use((err, req, res, next) => {
-//        console.log("========= MY LOG ===========",err); 
+//        console.log("========= MY LOG ===========",err);
 //        res.status(500).send({ msg: "Internal Server 33333rror" });
 // });
-
-
-
 
 app.use(errBadRequest);
 app.use(errTopics);
 app.use(errComments);
 
-module.exports = app; 
+module.exports = app;
